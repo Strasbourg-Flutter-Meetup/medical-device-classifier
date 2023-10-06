d5e373806ccbf4bfecdf1149496ba58f107cc0e1
DeeKaySxb <145773354+DeeKaySxb@users.noreply.github.com>
2023-10-06 10:41:05 +0200

    Chore/linter: updated linter  (#3)
    chore: Update analysis_options.yaml and add a documentation script
    
    * Updated the linters and code metrics.
    * The script allows on window machines to generate documentation.
    
    ---------
    
    Co-authored-by: Walnut IT <daniel.krentzlin@walnut-it.com>

---
+++ flutter_documentation_creator.bat
@@ -0,0 +1,20 @@
+echo Start to create documentation for the project LeoML Parser
+rmdir /s /q "doc"
+mkdir "doc"
+mkdir "coverage"
+echo Start to run the unit tests and to collect the code coverage
+call flutter test --machine > doc\machine.log --coverage
+echo Creating the coverage protocol as HTML
+call perl %GENHTML% -o doc\coverage\html coverage\lcov.info
+echo Creating the unit test report
+mkdir doc\test_report
+call flutter test --machine | tojunit > doc\test_report\junit_test_report.xml
+call junit-viewer --results=doc\test_report\junit_test_report.xml  --save=doc\test_report\junit_test_report.html --minify=false
+del doc\machine.log
+echo Starting code metrics
+call dart run dart_code_metrics:metrics analyze --reporter=html --no-fatal-warnings --output-directory=doc\code_metrics_report lib
+echo Starting code duplication detection
+call jscpd -o doc\jscpd --reporters html lib\
+echo Create documentation
+REM call dart doc -o doc\api .
+rmdir /s /q "coverage"
