e50c57f85934d3014446dcc1e3cbd4c3ffba13c6
Walnut IT <daniel.krentzlin@walnut-it.com>
2023-10-11 19:45:48 +0200

    feat: light and dark themes can be used now
    Depending on the systems setting the application is using
    the default light and dark themes.

--- lib/medical_device_classifier.dart
+++ lib/medical_device_classifier.dart
@@ -7,6 +7,7 @@
 // ID: 20231006105422
 // 06.10.2023 10:54
 
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:medical_device_classifier/routing/go_router_configuration.dart';
@@ -15,10 +16,17 @@ class MedicalDeviceClassifier extends StatelessWidget {
   const MedicalDeviceClassifier({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp.router(
-        title: 'Medical Device Classifier',
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        routerConfig: goRouterConfiguration,
+  Widget build(BuildContext context) => AdaptiveTheme(
+        light: ThemeData.light(useMaterial3: true),
+        dark: ThemeData.dark(useMaterial3: true),
+        initial: AdaptiveThemeMode.system,
+        builder: (ThemeData light, ThemeData dark) => MaterialApp.router(
+          title: 'Medical Device Classifier',
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          routerConfig: goRouterConfiguration,
+          theme: light,
+          darkTheme: dark,
+        ),
       );
 }
