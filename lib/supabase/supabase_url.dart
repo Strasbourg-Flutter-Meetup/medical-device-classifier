dbb970ca4218e5bd7f75f03602145e468837a42f
Walnut IT <daniel.krentzlin@walnut-it.com>
2023-10-12 22:54:13 +0200

    chore: remove files

--- lib/supabase/supabase_url.dart
+++
@@ -1,9 +0,0 @@
-// Project: Medical Device Classifier
-// Author: Daniel Krentzlin
-// Project begin: 05.10.2023
-// Dev Environment: Android Studio
-// Platform: Windows 11
-// Copyright: Strasbourg Flutter Meetup Group 2023
-// ID: 20231012202708
-// 12.10.2023 20:27
-const supabaseURL = 'https://eptgzylilgchnggpgzab.supabase.co';
